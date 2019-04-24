adec010b2155eca4758842f12325a6d870fc9661
Natthapon Sirinon <natthapon.sirinon@gmail.com>
2019-04-24 15:49:54 +0700

    adjust script

--- main.sh
+++ main.sh
@@ -1,18 +1,8 @@
-echo "=====>>> Execute Garbage Collection Script Running <<<=====" &&
-
-echo "=====>>> Clone Repository Running <<<=====" &&
-sudo git clone https://github.com/rentspree/gitlabci-garbage-collection.git &&
-echo "=====>>> Clone Repository Complete <<<=====" &&
-
-cd gitlabci-garbage-collection
-
-echo "=====>>> Start Garbage Collection with Crontab Running <<<=====" &&
-sudo docker run --rm --privileged -d \
+echo "=====>>> Start Garbage Collection to Crontab Running <<<=====" &&
+docker run --rm --privileged -d \
     --name rentspree-gc \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v $(pwd)/config:/opt/crontab:rw \
     -v $(pwd)/logs:/var/log/crontab:rw \
     willfarrell/crontab
-echo "=====>>> Start Garbage Collection with Crontab Complete <<<=====" &&
-
-echo "=====>>> Execute Garbage Collection Script Complete <<<=====" &&
+echo "=====>>> Start Garbage Collection to Crontab Complete <<<====="
