58308577e135d870e91733f1ed2bbb5a425dbe20
Natthapon Sirinon <natthapon.sirinon@gmail.com>
2019-04-24 16:01:51 +0700

    unmount volume log

--- main.sh
+++ main.sh
@@ -3,6 +3,5 @@ docker run --rm --privileged -d \
     --name rentspree-gc \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v $(pwd)/config:/opt/crontab:rw \
-    -v $(pwd)/logs:/var/log/crontab:rw \
     willfarrell/crontab
 echo "=====>>> Start Garbage Collection to Crontab Complete <<<====="
